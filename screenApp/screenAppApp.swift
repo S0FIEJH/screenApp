8ac4fc3089a1468be3407c06a764c6e0a3ce2931
S0FIEJH <Sofie_jasmijn_hoekendijk@s2025.ssts.edu.sg>
2026-07-04 10:05:13 +0800

    Initial Commit

---
+++ screenApp/screenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  screenAppApp.swift
+//  screenApp
+//
+//  Created by Sofie Hoekendijk on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct screenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
